c6e42ef11e762509b5019f9d86bfbf15b02dad4a
Nicole Padilla <npadilla8@bloomberg.net>
2023-09-14 18:50:00 -0400

    adding everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList.jsx'
+import ContactRow from './components/ContactRow.jsx'
 
-function App() {
+ export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+       <ContactList />
     </>
-  )
+  );
 }
 
-export default App
